02bc73448255ba078d24524a27e0ce4e54478e62
buxxi <buxxi@omfilm.net>
2017-12-20 17:59:50 +0100

    Fixing writing to HL not being treated as a word

--- gameboy-core/src/main/java/se/omfilm/gameboy/internal/instructions/load/LoadAIntoAddressOfHLIncreased.java
+++ gameboy-core/src/main/java/se/omfilm/gameboy/internal/instructions/load/LoadAIntoAddressOfHLIncreased.java
@@ -7,7 +7,7 @@ public class LoadAIntoAddressOfHLIncreased implements Instruction {
     public int execute(Memory memory, Registers registers, Flags flags, ProgramCounter programCounter, StackPointer stackPointer) {
         int hl = registers.readHL();
         memory.writeByte(hl, registers.readA());
-        registers.writeHL(hl + 1);
+        registers.writeHL((hl + 1) & 0xFFFF);
 
         return 8;
     }
--- gameboy-core/src/main/java/se/omfilm/gameboy/internal/instructions/load/LoadAddressOfHLIncreasedIntoA.java
+++ gameboy-core/src/main/java/se/omfilm/gameboy/internal/instructions/load/LoadAddressOfHLIncreasedIntoA.java
@@ -7,7 +7,7 @@ public class LoadAddressOfHLIncreasedIntoA implements Instruction {
     public int execute(Memory memory, Registers registers, Flags flags, ProgramCounter programCounter, StackPointer stackPointer) {
         int hl = registers.readHL();
         registers.writeA(memory.readByte(hl));
-        registers.writeHL(hl + 1);
+        registers.writeHL((hl + 1) & 0xFFFF);
         return 8;
     }
 }
