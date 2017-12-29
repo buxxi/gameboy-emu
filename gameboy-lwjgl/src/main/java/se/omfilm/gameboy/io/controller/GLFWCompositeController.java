4608f6ca4a4517317f62e206a072709e887ed9ec
buxxi <buxxi@omfilm.net>
2017-12-29 17:41:11 +0100

    Don't poll joypad input more than once per frame.

--- gameboy-lwjgl/src/main/java/se/omfilm/gameboy/io/controller/GLFWCompositeController.java
+++ gameboy-lwjgl/src/main/java/se/omfilm/gameboy/io/controller/GLFWCompositeController.java
@@ -1,13 +1,15 @@
 package se.omfilm.gameboy.io.controller;
 
 import org.lwjgl.glfw.GLFW;
+import se.omfilm.gameboy.io.screen.Screen;
 import se.omfilm.gameboy.io.screen.WindowChangeListener;
 
 import java.util.stream.IntStream;
 
 public class GLFWCompositeController implements Controller, WindowChangeListener {
     private final GLFWKeyboardController keyboard;
-    private Controller[] joypads = new Controller[0];
+    private GLFWJoypadController[] joypads = new GLFWJoypadController[0];
+    private long lastCheck = System.currentTimeMillis();
 
     public GLFWCompositeController() {
         keyboard = new GLFWKeyboardController();
@@ -27,9 +29,13 @@ public class GLFWCompositeController implements Controller, WindowChangeListener
     }
 
     public void update() {
+        if (System.currentTimeMillis() - lastCheck <= (Screen.FREQUENCY / 60)) { //Only perform the expensive check once every frame
+            return;
+        }
         for (int i = 0; i < joypads.length; i++) {
             joypads[i].update();
         }
+        lastCheck = System.currentTimeMillis();
     }
 
     public void windowChanged(long window) {
@@ -37,6 +43,6 @@ public class GLFWCompositeController implements Controller, WindowChangeListener
         joypads = IntStream.range(GLFW.GLFW_JOYSTICK_1, GLFW.GLFW_JOYSTICK_LAST + 1).
                 filter(GLFW::glfwJoystickPresent).
                 mapToObj(GLFWJoypadController::new).
-                toArray(Controller[]::new);
+                toArray(GLFWJoypadController[]::new);
     }
 }
